c9adceb852d10cec3c369f84c59ebcdf700c080c
Lars Holmberg <lars.holmberg@svenskaspel.se>
2019-09-30 17:34:36 +0200

    Stop whining about missing the RPS target for every locust/taskset, just say it once.

--- locust_plugins/tasksets.py
+++ locust_plugins/tasksets.py
@@ -9,6 +9,8 @@ class TaskSetRPS(TaskSet):
     A task set that allows locust to limit the number of requests by sleeping to reach a target RPS
     """
 
+    _failed_to_reach_rps_target = False
+
     def __init__(self, parent):
         super().__init__(parent)
         self._previous_time = 0.0
@@ -20,9 +22,9 @@ class TaskSetRPS(TaskSet):
             return
         next_time = self._previous_time + runners.locust_runner.num_clients / rps
         if current_time > next_time:
-            if runners.locust_runner.state == runners.STATE_RUNNING and not self._rps_fail:
+            if runners.locust_runner.state == runners.STATE_RUNNING and not TaskSetRPS._failed_to_reach_rps_target:
                 logging.warning("Failed to reach target rps, even after rampup has finished")
-                self._rps_fail = True  # stop logging
+                TaskSetRPS._failed_to_reach_rps_target = True  # stop logging
             self._previous_time = current_time
             return
 
