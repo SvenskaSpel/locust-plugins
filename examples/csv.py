8ac63dfc9d927d21102c8020f20cebb2d3d7eed2
Lars Holmberg <lars.holmberg@svenskaspel.se>
2019-10-04 15:29:07 +0200

    Add csv reader example

---
+++ examples/csv.py
@@ -0,0 +1,18 @@
+from locust import HttpLocust, TaskSet, task
+from locust_plugins.readers import CSVReader
+
+ssn_reader = CSVReader("ssn.csv")
+
+
+class MyTaskSet(TaskSet):
+    @task
+    def index(self):
+        customer = next(ssn_reader)
+        self.client.get(f"/?ssn={customer[0]}")
+
+
+class MyHttpLocust(HttpLocust):
+    task_set = MyTaskSet
+    min_wait = 100
+    max_wait = 100
+    host = "http://example.com"
