b1507b8dff8899195f54ab77e72ee23299791fe7
Lars Holmberg <lars.holmberg@svenskaspel.se>
2019-10-07 09:27:19 +0200

    Rename and simplify rps example.

--- examples/rps.py
+++ examples/rps.py
@@ -1,37 +1,28 @@
 #!/usr/bin/env python3
-import os
 import locust_plugins.utils
 
 locust_plugins.utils.gevent_debugger_patch()
 locust_plugins.utils.print_json_on_decode_fail()
 from locust_plugins.listeners import PrintListener, TimescaleListener
-from locust_plugins.readers import PostgresReader
 from locust_plugins.tasksets import TaskSetRPS
 from locust import HttpLocust, task
+import os
 
-TimescaleListener("example")
-
-customer_reader = PostgresReader(f"env='{os.environ['LOCUST_TEST_ENV']}'")
-
+TimescaleListener("rps_example")
 rps = float(os.environ["LOCUST_RPS"])
 
 
 class UserBehavior(TaskSetRPS):
     @task
     def my_task(self):
-        if rps:
-            self.rps_sleep(rps)
-        self.client.get("/")
-        customer = customer_reader.get()
-        self.client.post(f"/?ssn={customer['ssn']}")
-        customer_reader.release(customer)
+        self.rps_sleep(rps)
+        self.client.post("/authentication/1.0/getResults", {"username": "something"})
 
 
 class MyHttpLocust(HttpLocust):
     task_set = UserBehavior
     min_wait = 0
     max_wait = 0
-    host = "http://example.com"
     if __name__ == "__main__":
         _catch_exceptions = False
 
