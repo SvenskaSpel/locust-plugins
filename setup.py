282b73ead0ac70ceb18ef45abdbb47a092eb1628
Lars Holmberg <lars.holmberg@redshirt.se>
2019-10-19 23:42:26 +0200

    Just use latest version of locustio

--- setup.py
+++ setup.py
@@ -35,5 +35,5 @@ setup(
     packages=find_packages(exclude=["examples"]),
     include_package_data=True,
     zip_safe=False,
-    install_requires=["locustio==0.11.1", "psycogreen", "psycopg2", "websocket-client"],
+    install_requires=["locustio", "psycogreen", "psycopg2", "websocket-client"],
 )
