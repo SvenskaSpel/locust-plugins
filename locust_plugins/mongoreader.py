6983af4fb7eea9c39efeb0857df50ec2d3770d57
Lars Holmberg <lars.holmberg@svenskaspel.se>
2020-02-12 12:47:18 +0100

    filter for UNLOCKED customers, not locked ones. Doh.

--- locust_plugins/mongoreader.py
+++ locust_plugins/mongoreader.py
@@ -5,16 +5,16 @@ import time
 import os
 from contextlib import contextmanager
 
-connection_string = os.environ["LOCUST_MONGO"]
+uri = os.environ["LOCUST_MONGO"]
 collection = os.environ["LOCUST_MONGO_COLLECTION"]
 database = os.environ["LOCUST_MONGO_DATABASE"]
 
 
 class MongoReader:
     def __init__(self, filters):
-        self.coll = MongoClient(connection_string)[database][collection]
+        self.coll = MongoClient(uri)[database][collection]
         self.delay_warning = 0.5
-        self.query = {"$and": filters + [{"logged_in": 1}]}
+        self.query = {"$and": filters + [{"logged_in": 0}]}
 
     @contextmanager
     def customer(self):
@@ -22,18 +22,18 @@ class MongoReader:
         customer = self.coll.find_one_and_update(
             self.query, {"$set": {"last_login": datetime.now(), "logged_in": 1}}, sort=[("last_login", 1)]
         )
+        if customer is None:
+            raise Exception(f"Didnt get any customer from db using query {self.query}")
         if start_at + self.delay_warning < time.time():
             logging.warning(
                 f"Getting a customer took more than {self.delay_warning} seconds (doubling warning threshold for next time)"
             )
             self.delay_warning *= 2
-        if customer is None:
-            raise Exception(f"Didnt get any customer from db")
         try:
             yield customer
         finally:
             releasessn = self.coll.find_one_and_update(
-                {"$and": [{"ssn": customer["ssn"]}, {"logged_in": 1}]}, {"$set": {"logged_in": 0}},
+                {"$and": [{"ssn": customer["ssn"]}, {"logged_in": 1}]}, {"$set": {"logged_in": 0}}
             )
         if releasessn is None:
             raise Exception(f"Couldnt release lock for customer in db. ")
