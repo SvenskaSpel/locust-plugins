e897d465700a5cb4d8e4c5fc568283b29a77a47f
Lars Holmberg <lars.holmberg@svenskaspel.se>
2020-02-14 17:25:55 +0100

    change some naming, use parameters instead of directly relying on env vars. allow the user to supply id_column instead of assuming it is "ssn"

--- locust_plugins/mongoreader.py
+++ locust_plugins/mongoreader.py
@@ -5,35 +5,32 @@ import time
 import os
 from contextlib import contextmanager
 
-uri = os.environ["LOCUST_MONGO"]
-collection = os.environ["LOCUST_MONGO_COLLECTION"]
-database = os.environ["LOCUST_MONGO_DATABASE"]
-
 
 class MongoReader:
-    def __init__(self, filters):
+    def __init__(self, filters, id_column, uri, database, collection):
         self.coll = MongoClient(uri)[database][collection]
+        self.id_column = id_column
         self.delay_warning = 0.5
         self.query = {"$and": filters + [{"logged_in": 0}]}
 
     @contextmanager
-    def customer(self):
+    def user(self):
         start_at = time.time()
-        customer = self.coll.find_one_and_update(
+        user = self.coll.find_one_and_update(
             self.query, {"$set": {"last_login": datetime.now(), "logged_in": 1}}, sort=[("last_login", 1)]
         )
-        if customer is None:
-            raise Exception(f"Didnt get any customer from db using query {self.query}")
+        if user is None:
+            raise Exception(f"Didnt get any user from db ({self.coll}) using query {self.query}")
         if start_at + self.delay_warning < time.time():
             logging.warning(
-                f"Getting a customer took more than {self.delay_warning} seconds (doubling warning threshold for next time)"
+                f"Getting a user took more than {self.delay_warning} seconds (doubling warning threshold for next time)"
             )
             self.delay_warning *= 2
         try:
-            yield customer
+            yield user
         finally:
             releasessn = self.coll.find_one_and_update(
-                {"$and": [{"ssn": customer["ssn"]}, {"logged_in": 1}]}, {"$set": {"logged_in": 0}}
+                {"$and": [{self.id_column: user[self.id_column]}, {"logged_in": 1}]}, {"$set": {"logged_in": 0}}
             )
         if releasessn is None:
-            raise Exception(f"Couldnt release lock for customer in db. ")
+            raise Exception(f"Couldnt release lock for user in db. ")
