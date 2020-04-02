9b59f2a8d0744af6ba7e678b9b4cd7bc81981c33
Lars Holmberg <lars.holmberg@svenskaspel.se>
2020-04-02 11:24:49 +0200

    fix some kafka stuff

--- examples/kafka_test.py
+++ examples/kafka_test.py
@@ -1,21 +1,24 @@
 from locust_plugins.debug import run_single_user
 from locust_plugins.kafka import KafkaLocust
-from locust import task
+from locust import task, TaskSet
 from locust.wait_time import constant
 import time
 import random
 import os
 
 
-class MyLocust(KafkaLocust):
-    bootstrap_servers = os.environ["LOCUST_KAFKA_SERVERS"].split(",")
-
+class MyTaskSet(TaskSet):
     @task
     def t(self):
         self.client.send("lafp_test", message=f"{time.time() * 1000}:" + ("kafka" * 24)[: random.randint(32, 128)])
         self.client.producer.flush(timeout=5)
         time.sleep(1)
 
+
+class MyLocust(KafkaLocust):
+    bootstrap_servers = os.environ["LOCUST_KAFKA_SERVERS"].split(",")
+    tasks = {MyTaskSet: 1}
+    task_set = MyTaskSet
     wait_time = constant(0)
 
 
