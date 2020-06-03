5f02a737dbe5b0575d6faa66feee99cbafd35d51
Howard Osborne <howardosborneconsulting@gmail.com>
2020-06-03 07:35:58 +0100

    added suggestions in PR

--- examples/embedded_resource_manager_ex.py
+++ examples/embedded_resource_manager_ex.py
@@ -1,19 +1,16 @@
-import sys, os
-sys.path.append(os.getcwd())
 from locust_plugins.embedded_resource_manager import HttpUserWithResources
-from locust import task, events
-import time
+from locust import task
 
 class TestUserWithResources(HttpUserWithResources):
-    bundle_resource_stats=False
-    default_resource_filter=".*[^(js)]$"
+    #bundle_resource_stats=False
+    #default_resource_filter=".*[^(js)]$"
+    @task
+    def include_resources_default(self):
+        response = self.client.get("/cart.html")
+
     @task
     def include_resources_true(self):
         response = self.client.get("/", resource_filter=".*[^(js)]$")
-
-    @task
-    def include_resources_missing(self):
-        response = self.client.get("/cart.html")
 
     @task
     def include_resources_false(self):
--- locust_plugins/embedded_resource_manager.py
+++ locust_plugins/embedded_resource_manager.py
@@ -12,10 +12,10 @@ class HttpUserWithResources(HttpUser):
 
     abstract = True
 
-    include_resources_by_default = True
-    default_resource_filter = ".*"
-    bundle_resource_stats = True
-    cache_resource_links = True
+    include_resources_by_default=True
+    default_resource_filter=".*"
+    bundle_resource_stats=True
+    cache_resource_links=True
 
     def __init__(self, *args):
         super().__init__(*args)
@@ -35,10 +35,10 @@ class FastHttpUserWithResources(FastHttpUser):
 
     abstract = True
 
-    include_resources_by_default = True
-    default_resource_filter = ".*"
-    bundle_resource_stats = True
-    cache_resource_links = True
+    include_resources_by_default=True
+    default_resource_filter=".*"
+    bundle_resource_stats=True
+    cache_resource_links=True
 
     def __init__(self, *args):
         super().__init__(*args)
@@ -143,19 +143,7 @@ class EmbeddedResourceManager:
         return resources
 
     def _request(self, func):
-        def wrapper(*args, **kwargs):
-            # check if include_resources flag set
-            if "include_resources" in kwargs:
-                include_resources = kwargs["include_resources"]
-                del kwargs["include_resources"]
-            else:
-                include_resources = self.include_resources
-
-            if "resource_filter" in kwargs:
-                resource_filter = kwargs["resource_filter"]
-                del kwargs["resource_filter"]
-            else:
-                resource_filter = self.resource_filter_pattern
+        def wrapper(*args, include_resources=self.include_resources, resource_filter=self.resource_filter_pattern, **kwargs):
 
             response = func(*args, **kwargs)
 
