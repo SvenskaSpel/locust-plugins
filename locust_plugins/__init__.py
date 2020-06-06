601625a8d1a4e10a5f26167f88cfdf736ff0562b
Lars Holmberg <lars.holmberg@svenskaspel.se>
2020-06-06 19:32:49 +0200

    remove extra newlines

--- locust_plugins/__init__.py
+++ locust_plugins/__init__.py
@@ -8,5 +8,3 @@ from locust import User, constant
 # Monkey patch User while waiting for everyone else to see the light:
 # https://github.com/locustio/locust/issues/1308
 User.wait_time = constant(0)
-
-
