a29347029cfb14616aee9b0b31e5a3bf2451bcf4
Lars Holmberg <lars.holmberg@svenskaspel.se>
2020-06-06 20:11:19 +0200

    use example domain that actually works atm. Break after one iteration (so we can run this from tox)

--- examples/debug_ex.py
+++ examples/debug_ex.py
@@ -1,15 +1,17 @@
 # How to use VS Code debugger with Locust
 from locust import task, HttpUser
+from locust.exception import StopUser
 from locust_plugins import run_single_user
 
 
 class MyUser(HttpUser):
     @task
     def t(self):
-        self.client.post("/")
+        self.client.get("/")
+        raise StopUser()
 
 
 # when executed as a script, run a single locust in a way suitable for the vs code debugger
 if __name__ == "__main__":
-    MyUser.host = "http://example.com"
+    MyUser.host = "http://example.edu"
     run_single_user(MyUser)
