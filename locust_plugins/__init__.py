770c8ae0371e3394cd9b5978e2e816b6a1024f7c
Lars Holmberg <lars.holmberg@svenskaspel.se>
2020-11-24 13:12:57 +0100

    Locust now defaults to zero wait time, no need to monkey patch

--- locust_plugins/__init__.py
+++ locust_plugins/__init__.py
@@ -10,10 +10,6 @@ from locust.runners import Runner
 import logging
 from functools import wraps
 
-# Monkey patch User while waiting for everyone else to see the light:
-# https://github.com/locustio/locust/issues/1308
-User.wait_time = constant(0)
-
 
 @events.init_command_line_parser.add_listener
 def add_checks_arguments(parser):
