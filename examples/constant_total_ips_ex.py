f65811a06619d5dfe8409207ac013b426a49d89b
Lars Holmberg <lars.holmberg@svenskaspel.se>
2021-02-09 08:24:41 +0100

    Simplify constant total ips example

--- examples/constant_total_ips_ex.py
+++ examples/constant_total_ips_ex.py
@@ -1,21 +1,16 @@
-from locust import HttpUser, task, events
+from locust import HttpUser, task
 from locust_plugins import run_single_user, constant_total_ips
-from locust_plugins.listeners import TimescaleListener
 
 
 class MyUser(HttpUser):
+    wait_time = constant_total_ips(2)
+
     @task
     def my_task(self):
         self.client.get("/")
 
-    wait_time = constant_total_ips(5)
     host = "https://www.example.com"
 
 
-@events.init.add_listener
-def on_locust_init(environment, **_kwargs):
-    TimescaleListener(env=environment, testplan="constant_total_ips", target_env="myTestEnv")
-
-
 if __name__ == "__main__":
-    run_single_user(MyUser)
+    run_single_user(MyUser, include_time=True)
