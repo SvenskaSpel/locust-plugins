7b5dae406014dd3bdd61aebe10dc3b79293e5106
Lars Holmberg <lars.holmberg@svenskaspel.se>
2021-02-09 10:57:44 +0100

    better example of constant_total_ips

--- examples/constant_total_ips_ex.py
+++ examples/constant_total_ips_ex.py
@@ -1,16 +1,15 @@
+# this will run approximately 5 iterations per second, regardless of the number of users or response times
+# (assuming response times are low enough / user count is high enough to make that throughput possible)
+
 from locust import HttpUser, task
-from locust_plugins import run_single_user, constant_total_ips
+from locust_plugins import constant_total_ips
 
 
 class MyUser(HttpUser):
-    wait_time = constant_total_ips(2)
+    wait_time = constant_total_ips(5)
 
     @task
-    def my_task(self):
-        self.client.get("/")
-
-    host = "https://www.example.com"
-
-
-if __name__ == "__main__":
-    run_single_user(MyUser, include_time=True)
+    def t(self):
+        self.client.get("/1")
+        self.client.get("/2")
+        # because we do two requests in each iteration, the total request rate will be ~10/s
