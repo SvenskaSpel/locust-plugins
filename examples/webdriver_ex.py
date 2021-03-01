d2ec85bd101b76de663e3d65c28be8002c1ccde0
Lars Holmberg <lars.holmberg@svenskaspel.se>
2021-03-01 10:36:17 +0100

    updates for WebdriverUser & example

--- examples/webdriver_ex.py
+++ examples/webdriver_ex.py
@@ -2,6 +2,8 @@
 # Download it from https://www.seleniumhq.org/download/ and run it by executing:
 # java -jar selenium-server-4.0.0-beta-1.jar standalone
 import time
+
+from locust.user.wait_time import constant
 from locust_plugins import run_single_user
 from locust_plugins.users import WebdriverUser
 from locust import task
@@ -11,6 +13,8 @@ from selenium.common.exceptions import StaleElementReferenceException
 
 
 class MyUser(WebdriverUser):
+    wait_time = constant(2)
+
     def __init__(self, parent):
         super().__init__(parent, headless=(__name__ != "__main__"))
 
@@ -22,6 +26,7 @@ class MyUser(WebdriverUser):
     @task
     def my_task(self):
         self.client.delete_all_cookies()
+        self.client.start_time = time.time()
         self.client.get("https://spela.test4.svenskaspel.se/")
         self.client.add_cookie(
             {
@@ -33,20 +38,25 @@ class MyUser(WebdriverUser):
         )
         start_at = time.monotonic()
         self.client.get("https://spela.test4.svenskaspel.se/logga-in/bankid/ssn")
-        ssn_input = self.client.find_element(By.CSS_SELECTOR, "#ssn")
+        ssn_input = self.client.find_element(By.CSS_SELECTOR, "#ssn", name="ssn page ready")
         ssn_input.click()
         ssn_input.send_keys("199901010109")
         ssn_input.send_keys(Keys.RETURN)
         self.client.implicitly_wait(10)
         try:
-            self.client.find_element(By.XPATH, '//*[@id="last-login-time"]/div/div[4]/a/span').click()
+            self.client.find_element(
+                By.XPATH, '//*[@id="last-login-time"]/div/div[4]/a/span', name="login complete"
+            ).click()
         except StaleElementReferenceException:
             # retry...
-            self.client.find_element(By.XPATH, '//*[@id="last-login-time"]/div/div[4]/a/span').click()
+            self.client.find_element(
+                By.XPATH, '//*[@id="last-login-time"]/div/div[4]/a/span', name="login complete"
+            ).click()
         # show balance
         self.client.find_element(
             By.CSS_SELECTOR,
             "body > div.fixed-top-content.js-top-content-wrapper.balance-bar-ao-brand-small > div.balance-bar-account > span.balance-bar-account-item.balance-bar-left-border.pointer.js-balance-toggle.balance-bar-toggle > span",
+            name="balance visible",
         ).click()
 
         self.environment.events.request_success.fire(
--- locust_plugins/users/webdriver.py
+++ locust_plugins/users/webdriver.py
@@ -1,16 +1,42 @@
+# See examples/webdriver_ex.py for more documentation
 import subprocess
+import time
 from locust import User
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.chrome.options import Options
 
 
+class WebdriverClient(webdriver.Remote):
+    def __init__(self, environment, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._locust_environment = environment
+        self.start_time = None
+
+    def find_element(self, *args, name=None, **kwargs):  # pylint: disable=arguments-differ
+        name = name or args[1]
+        if not self.start_time:
+            self.start_time = time.time()
+        try:
+            result = super().find_element(*args, **kwargs)
+        except Exception as e:
+            total_time = int((time.time() - self.start_time) * 1000)
+            self._locust_environment.events.request_failure.fire(
+                request_type="Selenium", name=name, response_time=total_time, exception=e
+            )
+            self.start_time = None
+            if not isinstance(e, WebDriverException):
+                raise
+        else:
+            total_time = int((time.time() - self.start_time) * 1000)
+            self._locust_environment.events.request_success.fire(
+                request_type="Selenium", name=name, response_time=total_time, response_length=0
+            )
+            self.start_time = None
+        return result
+
+
 class WebdriverUser(User):
-    """
-    A locust that includes a Webdriver client.
-    Download & launch selenium server first:
-    https://www.seleniumhq.org/download/
-    java -jar selenium-server-standalone-3.141.59.jar
-    """
 
     abstract = True
     _first_instance = True
@@ -26,6 +52,8 @@ class WebdriverUser(User):
         chrome_options = Options()
         if headless:
             chrome_options.add_argument("--headless")
-        self.client = webdriver.Remote(
-            command_executor="http://127.0.0.1:4444/wd/hub", desired_capabilities=chrome_options.to_capabilities()
+        self.client = WebdriverClient(
+            self.environment,
+            command_executor="http://127.0.0.1:4444/wd/hub",
+            desired_capabilities=chrome_options.to_capabilities(),
         )
