8332bd1f7dc4d950120e832fdf82da5f4e6fa776
Lars Holmberg <lars.holmberg@svenskaspel.se>
2021-03-14 19:37:19 +0100

    explicitly support 3.8

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Intended Audience :: Developers",
         "Intended Audience :: System Administrators",
     ],
