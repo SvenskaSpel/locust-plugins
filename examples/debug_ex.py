7190265f908cc58dc324c0d4cbaac75962fe1bfc
Lars Holmberg <lars.holmberg@svenskaspel.se>
2021-03-15 15:27:26 +0100

    add comment about silly debugger warnings

--- examples/debug_ex.py
+++ examples/debug_ex.py
@@ -1,5 +1,17 @@
 # Example of how to use VS Code debugger with Locust.
 # Make sure you set "gevent": true in your launch.json
+
+# In recent versions of VSCode you might get the following warning. I dont know why, but it seems safe to ignore.
+#
+# PYDEV DEBUGGER WARNING:
+# sys.settrace() should not be used when the debugger is being used.
+# This may cause the debugger to stop working correctly.
+# If this is needed, please check:
+# http://pydev.blogspot.com/2007/06/why-cant-pydev-debugger-work-with.html
+# to see how to restore the debug tracing back correctly.
+#
+# (if you know why this happens, please let me know :)
+
 from locust import task, HttpUser
 from locust.exception import StopUser
 from locust_plugins import run_single_user
