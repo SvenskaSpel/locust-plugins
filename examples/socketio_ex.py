3a2498ee81695171359626e82ba2e2413e0a14fe
Lars Holmberg <lars.holmberg@redshirt.se>
2021-08-09 23:01:43 +0200

    Update SocketIOUser and its example

--- examples/socketio_ex.py
+++ examples/socketio_ex.py
@@ -1,3 +1,5 @@
+import time
+import json
 from locust import task
 from locust_plugins.users import SocketIOUser
 
@@ -5,13 +7,25 @@ from locust_plugins.users import SocketIOUser
 class MySocketIOUser(SocketIOUser):
     @task
     def my_task(self):
+        self.my_value = None
+
         self.connect("wss://example.com/socket.io/?EIO=3&transport=websocket")
+
         # example of subscribe
         self.send('42["subscribe",{"url":"/sport/matches/11995208/draws","sendInitialUpdate": true}]')
+
+        # wait until I get a push message to on_message
+        while not self.my_value:
+            time.sleep(0.1)
+
         # you can do http in the same taskset as well
         self.client.get("/")
-        # wait for pushes, while occasionally sending heartbeats, like a real client would
+
+        # wait for additional pushes, while occasionally sending heartbeats, like a real client would
         self.sleep_with_heartbeat(10)
+
+    def on_message(self, message):
+        self.my_value = json.loads(message)["my_value"]
 
     if __name__ == "__main__":
         host = "http://example.com"
--- locust_plugins/users/socketio.py
+++ locust_plugins/users/socketio.py
@@ -18,84 +18,89 @@ class SocketIOUser(User):
 
     abstract = True
 
-    def connect(self, host, header):
+    def connect(self, host: str, header: list):
         self.ws = websocket.create_connection(host, header=header)
-        gevent.spawn(self.receive)
+        gevent.spawn(self.receive_loop)
 
-    def receive(self):
-        message_regex = re.compile(r"(\d*)(.*)")
-        description_regex = re.compile(r"<([0-9]+)>$")
-        response_time = 0
+    message_regex = re.compile(r"(\d*)(.*)")
+    description_regex = re.compile(r"<([0-9]+)>$")
+
+    def on_message(self, message):  # override this method in your subclass for custom handling
+        m = self.message_regex.match(message)
+        if m is None:
+            # uh oh...
+            raise Exception(f"got no matches in {message}")
+        code = m.group(1)
+        json_string = m.group(2)
+        if code == "0":
+            name = "0 open"
+        elif code == "3":
+            name = "3 heartbeat"
+        elif code == "40":
+            name = "40 message ok"
+        elif code == "42":
+            # this is rather specific to our use case. Some messages contain an originating timestamp,
+            # and we use that to calculate the delay & report it as locust response time
+            # see it as inspiration rather than something you just pick up and use
+            current_timestamp = time.time()
+            obj = json.loads(json_string)
+            logging.debug(json_string)
+            ts_type, payload = obj
+            name = f"{code} {ts_type} apiUri: {payload['apiUri']}"
+
+            if payload["value"] != "":
+                value = payload["value"]
+
+                if "draw" in value:
+                    description = value["draw"]["description"]
+                    description_match = self.description_regex.search(description)
+                    if description_match:
+                        sent_timestamp = int(description_match.group(1))
+                        response_time = current_timestamp - sent_timestamp
+                    else:
+                        # differentiate samples that have no timestamps from ones that do
+                        name += "_"
+                elif "source_ts" in value:
+                    sent_timestamp = value["source_ts"]
+                    response_time = (current_timestamp - sent_timestamp) * 1000
+            else:
+                name += "_missingTimestamp"
+        else:
+            print(f"Received unexpected message: {message}")
+            return
+        self.environment.events.request.fire(
+            request_type="WSR",
+            name=name,
+            response_time=response_time,
+            response_length=len(message),
+            exception=None,
+            context={},
+        )
+
+    def receive_loop(self):
         while True:
             message = self.ws.recv()
             logging.debug(f"WSR: {message}")
-            m = message_regex.match(message)
-            if m is None:
-                # uh oh...
-                raise Exception(f"got no matches in {message}")
-            code = m.group(1)
-            json_string = m.group(2)
-            if code == "0":
-                name = "0 open"
-            elif code == "3":
-                name = "3 heartbeat"
-            elif code == "40":
-                name = "40 message ok"
-            elif code == "42":
-                # this is rather specific to our use case. Some messages contain an originating timestamp,
-                # and we use that to calculate the delay & report it as locust response time
-                # see it as inspiration rather than something you just pick up and use
-                current_timestamp = time.time()
-                obj = json.loads(json_string)
-                logging.debug(json_string)
-                ts_type, payload = obj
-                name = f"{code} {ts_type} apiUri: {payload['apiUri']}"
+            self.on_message(message)
 
-                if payload["value"] != "":
-                    value = payload["value"]
-
-                    if "draw" in value:
-                        description = value["draw"]["description"]
-                        description_match = description_regex.search(description)
-                        if description_match:
-                            sent_timestamp = int(description_match.group(1))
-                            response_time = current_timestamp - sent_timestamp
-                        else:
-                            # differentiate samples that have no timestamps from ones that do
-                            name += "_"
-                    elif "source_ts" in value:
-                        sent_timestamp = value["source_ts"]
-                        response_time = (current_timestamp - sent_timestamp) * 1000
-                else:
-                    name += "_missingTimestamp"
+    def send(self, body, name=None, context={}):
+        if not name:
+            if body == "2":
+                name = "2 heartbeat"
             else:
-                print(f"Received unexpected message: {message}")
-                continue
-            self.environment.events.request.fire(
-                request_type="WSR",
-                name=name,
-                response_time=response_time,
-                response_length=len(message),
-                exception=None,
-                context={},
-            )
-
-    def send(self, body, context={}):
-        if body == "2":
-            action = "2 heartbeat"
-        else:
-            m = re.search(r'(\d*)\["([a-z]*)"', body)
-            assert m is not None
-            code = m.group(1)
-            action = m.group(2)
-            url_part = re.search(r'"url": *"([^"]*)"', body)
-            assert url_part is not None
-            url = re.sub(r"/[0-9_]*/", "/:id/", url_part.group(1))
-            action = f"{code} {action} url: {url}"
+                # hoping this is a subscribe type message, try to detect name
+                m = re.search(r'(\d*)\["([a-z]*)"', body)
+                assert m is not None
+                code = m.group(1)
+                action = m.group(2)
+                url_part = re.search(r'"url": *"([^"]*)"', body)
+                assert url_part is not None
+                url = re.sub(r"/[0-9_]*/", "/:id/", url_part.group(1))
+                name = f"{code} {action} url: {url}"
 
         self.environment.events.request.fire(
             request_type="WSS",
-            name=action,
+            name=name,
             response_time=None,
             response_length=len(body),
             exception=None,
