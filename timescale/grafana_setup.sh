2ed7147d4c32ea867156b48732fdd0a6a6b2c39f
Lars Holmberg <lars.holmberg@svenskaspel.se>
2021-12-01 14:00:12 +0100

    Call the database "postgres", not "timescale"

--- timescale/grafana_setup.sh
+++ timescale/grafana_setup.sh
@@ -3,9 +3,8 @@
 # edit this as necessary, if you want to set up the DS/dashboards on a different environment
 export GRAFANA_HOST="http://grafana:3000"
 export GRAFANA_CRED="admin:admin"
-export GRAFANA_DATASOURCE="timescale"
-export TIMESCALE_HOST="localhost"
-export TIMESCALE_PORT="5432"
+export POSTGRES_HOST="postgres"
+export POSTGRES_PORT="5432"
 
 ${BASH_SOURCE%/*}/create_datasource.sh
 ${BASH_SOURCE%/*}/import_dashboards.sh
