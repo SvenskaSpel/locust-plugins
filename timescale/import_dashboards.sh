bd3df6fe7ef95030bdda3a7913ad985a7f2dc6b2
Lars Holmberg <lars.holmberg@svenskaspel.se>
2021-12-03 14:55:27 +0100

    Simplify some stuff.

--- timescale/import_dashboards.sh
+++ timescale/import_dashboards.sh
@@ -3,12 +3,8 @@ ds=(10878 14423 14422);
 for d in "${ds[@]}"; do
   echo -n "Processing $d: "
   j=$(curl -s -k -u "$GRAFANA_CRED" $GRAFANA_HOST/api/gnet/dashboards/$d | jq .json)
-  curl -s -k -u "$GRAFANA_CRED" -XPOST -H "Accept: application/json" \
-    -H "Content-Type: application/json" \
-    -d "{\"dashboard\":$j,\"overwrite\":true, \
-        \"inputs\":[
-            {\"name\":\"VAR_SEND_INTERVAL\",\"type\":\"constant\",\"value\": \"5\"},\
-            {\"name\":\"DS_LOCUST\",\"type\":\"datasource\", \"pluginId\":\"postgres\",\"value\":\"$DS_NAME\"}\
-        ]}"\
+  curl -s -k -u "$GRAFANA_CRED" -XPOST -H "Accept: application/json"\
+    -H "Content-Type: application/json"\
+    -d "{\"dashboard\":$j,\"overwrite\":true,\"inputs\":[{\"name\":\"DS_LOCUST\",\"type\":\"datasource\", \"pluginId\":\"postgres\",\"value\":\"$DS_NAME\"}]}"\
     $GRAFANA_HOST/api/dashboards/import; echo ""
 done
