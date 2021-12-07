774cee6b3251c31a439fd4af34ab4985091f1066
Lars Holmberg <lars.holmberg@svenskaspel.se>
2021-12-07 14:04:58 +0100

    Re-add meaningless comments in schema

--- locust_plugins/timescale/locust-timescale/timescale_schema.sql
+++ locust_plugins/timescale/locust-timescale/timescale_schema.sql
@@ -1,3 +1,10 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 11.3
+-- Dumped by pg_dump version 11.3
+
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
