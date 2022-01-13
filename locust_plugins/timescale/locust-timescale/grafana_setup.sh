0caa36db68d1b4520688edfb867c57d4e5a50a5a
Lars Holmberg <lars.holmberg@svenskaspel.se>
2022-01-13 21:20:34 +0100

    grafana_setup.sh: Allow env vars to override grafana settings

--- locust_plugins/timescale/locust-timescale/grafana_setup.sh
+++ locust_plugins/timescale/locust-timescale/grafana_setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 
 # edit this as necessary, if you want to set up the DS/dashboards on a different environment
-export GRAFANA_HOST="http://grafana:3000"
-export GRAFANA_CRED="admin:admin"
-export GRAFANA_OVERWRITE="false" # change to true to overwrite pre-existing dashboards (update to latest version from grafana.com)
+export GRAFANA_HOST="${GRAFANA_HOST:=http://grafana:3000}"
+export GRAFANA_CRED="${GRAFANA_CRED:=admin:admin}"
+export GRAFANA_OVERWRITE="${GRAFANA_OVERWRITE:=false}" # change to true to overwrite pre-existing dashboards (update to latest version from grafana.com)
 export PGHOST="${PGHOST:=postgres}"
 export PGPORT="${PGPORT:=5432}"
-export DS_NAME="locust_timescale"
+export DS_NAME="${DS_NAME:=locust_timescale}"
 
 ${BASH_SOURCE%/*}/create_datasource.sh
 ${BASH_SOURCE%/*}/import_dashboards.sh
