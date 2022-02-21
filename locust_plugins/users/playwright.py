fd3e030ebd2120f680c787199416df9899cf49c8
Lars Holmberg <lars.holmberg@svenskaspel.se>
2022-02-21 09:58:57 +0100

    various playwrigth updates

--- locust_plugins/users/playwright.py
+++ locust_plugins/users/playwright.py
@@ -8,6 +8,7 @@ from contextlib import asynccontextmanager
 import os
 import asyncio
 from locust import User, events, task
+from locust.runners import WorkerRunner
 import gevent
 import sys
 import ast
@@ -17,9 +18,14 @@ import os
 import re
 from locust.exception import CatchResponseError
 import playwright as pw
-import re
 
 loop: asyncio.AbstractEventLoop = None
+
+# import yappi
+
+# yappi.set_context_backend("greenlet")
+# yappi.set_clock_type("wall")
+# yappi.start(builtins=True)
 
 
 def sync(async_func):
@@ -39,37 +45,60 @@ def sync(async_func):
 
 
 @asynccontextmanager
-async def event(user: "PlaywrightUser", name="unnamed", request_type="event"):
-    task_start_time = time.time()
-    start_perf_counter = time.perf_counter()
+async def event(
+    user: "PlaywrightUser",
+    name="unnamed",
+    request_type="event",
+    max_attempts=0,
+    # internals
+    attempt=0,
+    start_time=None,
+    start_perf_counter=None,
+):
+    if start_time is None:
+        start_time = time.time()
+    if start_perf_counter is None:
+        start_perf_counter = time.perf_counter()
     try:
         yield
         user.environment.events.request.fire(
             request_type=request_type,
             name=name,
-            start_time=task_start_time,
+            start_time=start_time,
             response_time=(time.perf_counter() - start_perf_counter) * 1000,
             response_length=0,
             context={**user.context()},
+            url=user.page.url if user.page else None,
             exception=None,
         )
     except Exception as e:
+        # if isinstance(e, AssertionError):
+        #     if max_attempts > attempt:
+        #         with user.event(
+        #             name, max_attempts, attempt + 1, start_time=start_time, start_perf_counter=start_perf_counter
+        #         ):
+        #             yield
         try:
             error = CatchResponseError(re.sub("=======*", "", e.message).replace("\n", "").replace(" logs ", " "))
         except:
             error = e  # never mind
         if not user.error_screenshot_made:
             user.error_screenshot_made = True  # dont spam screenshots...
-            await user.page.screenshot(path="screenshot_" + time.strftime("%H%M%S") + ".png")
+            if user.page:  # in ScriptUser runs we have no reference to the page so...
+                await user.page.screenshot(
+                    path="screenshot_" + time.strftime("%Y%m%d_%H%M%S") + ".png", full_page=False
+                )
         user.environment.events.request.fire(
-            request_type="TASK",
+            request_type=request_type,
             name=name,
-            start_time=task_start_time,
+            start_time=start_time,
             response_time=(time.perf_counter() - start_perf_counter) * 1000,
             response_length=0,
+            url=user.page.url if user.page else None,
             context={**user.context()},
             exception=error,
         )
+    await asyncio.sleep(0.1)
 
 
 def pw(func):
@@ -89,26 +118,65 @@ def pw(func):
             if user.browser is None:
                 user.browser = await user.playwright.chromium.launch(
                     headless=user.headless or user.headless is None and user.environment.runner is not None,
-                    # channel="chrome",
                     args=[
                         "--disable-gpu",
-                        # "--no-sandbox",
-                        # "--disable-setuid-sandbox",
-                        # "--disable-accelerated-2d-canvas",
-                        # "--no-first-run",
-                        # "--no-zygote",
-                        # "--single-process",
+                        "--disable-setuid-sandbox",
+                        "--disable-accelerated-2d-canvas",
+                        "--no-zygote",
+                        # "--frame-throttle-fps=10",
+                        # didnt seem to help much:
+                        "--single-process",
+                        #
+                        "--enable-profiling",
+                        "--profiling-at-start=renderer",
+                        "--no-sandbox",
+                        "--profiling-flush",
+                        # maybe even made it worse?
+                        # "--disable-gpu-vsync",
+                        # "--disable-site-isolation-trials",
+                        # "--disable-features=IsolateOrigins",
+                        #
+                        # maybe a little better?
+                        "--disable-blink-features=AutomationControlled",
+                        "--disable-blink-features",
+                        "--disable-translate",
+                        "--safebrowsing-disable-auto-update",
+                        "--disable-sync",
+                        "--hide-scrollbars",
+                        "--disable-notifications",
+                        "--disable-logging",
+                        "--disable-permissions-api",
+                        "--ignore-certificate-errors",
+                        # made no difference
+                        "--proxy-server='direct://'",
+                        "--proxy-bypass-list=*",
+                        # seems to help a little?
+                        "--blink-settings=imagesEnabled=false",
+                        # "--profile-directory=tmp/chromium-profile-dir-"
+                        # + "".join(random.choices("abcdef" + string.digits, k=8)),
+                        "--host-resolver-rules=MAP www.googletagmanager.com 127.0.0.1, MAP www.google-analytics.com 127.0.0.1, MAP *.facebook.* 127.0.0.1, MAP assets.adobedtm.com 127.0.0.1, MAP s2.adform.net 127.0.0.1",
+                        "--no-first-run",
+                        #
+                        "--disable-audio-output",
+                        "--disable-canvas-aa",
                     ],
-                    ignore_default_args=["--disable-dev-shm-usage"],  # we have plenty of space on /dev/shm
+                    # we have plenty of space on /dev/shm, and this was causing issues for us, so skip that:
+                    ignore_default_args=["--disable-dev-shm-usage"],
                 )
-            # I wish we could call this just "context" but it would collide with User.context():
-            user.browser_context = await user.browser.new_context()
-            user.page = await user.browser_context.new_page()
+                # I wish we could call this just "context" but it would collide with User.context():
+                user.browser_context = await user.browser.new_context(ignore_https_errors=True, base_url=user.host)
+                # await user.browser_context.add_init_script("() => delete window.navigator.serviceWorker")
+                user.page = await user.browser_context.new_page()
+                user.page.set_default_timeout(60_000)
+            else:
+                # cheap way to pretend we're a new user (instead of creating a whole new BrowserContext and Page
+                await user.browser_context.clear_cookies()
+
             name = user.__class__.__name__ + "." + func.__name__
         try:
             task_start_time = time.time()
             start_perf_counter = time.perf_counter()
-            await func(user)
+            await func(user, user.page)
             user.environment.events.request.fire(
                 request_type="TASK",
                 name=name,
@@ -117,15 +185,21 @@ def pw(func):
                 response_length=0,
                 context={**user.context()},
                 exception=None,
+                # url=user.page.url,
             )
         except Exception as e:
             try:
-                error = CatchResponseError(re.sub("=======*", "", e.message).replace("\n", "").replace(" logs ", " "))
+                error = CatchResponseError(
+                    re.sub("=======*", "", e.message + user.page.url).replace("\n", "").replace(" logs ", " ")
+                )
             except:
                 error = e  # never mind
             if not user.error_screenshot_made:
                 user.error_screenshot_made = True  # dont spam screenshots...
-                await user.page.screenshot(path="screenshot_" + time.strftime("%H%M%S") + ".png")
+                if user.page:  # in ScriptUser runs we have no reference to the page so...
+                    await user.page.screenshot(
+                        path="screenshot_" + time.strftime("%Y%m%d_%H%M%S") + ".png", full_page=True
+                    )
             user.environment.events.request.fire(
                 request_type="TASK",
                 name=name,
@@ -134,11 +208,13 @@ def pw(func):
                 response_length=0,
                 context={**user.context()},
                 exception=error,
+                url=user.page.url if user.page else None,
             )
         finally:
             if not isinstance(user, PlaywrightScriptUser):
-                await user.page.close()
-                await user.browser_context.close()
+                await user.page.wait_for_timeout(1000)  # give outstanding interactions some time
+                # await user.page.close()
+                # await user.browser_context.close()
 
     return pwwrapFunc
 
@@ -168,6 +244,9 @@ class PlaywrightScriptUser(PlaywrightUser):
                 p.body.remove(node)  # remove "asyncio.run(main())"
             elif isinstance(node, ast.AsyncFunctionDef) and node.name == "run":
                 # future optimization: reuse browser instances
+                # page_arg = ast.arg(arg="page")
+                # node.args.args.append(page_arg)
+                # ast.fix_missing_locations(node)
                 launch_line = node.body[0]  # browser = await playwright.chromium.launch(headless=False)
                 # default is for full Locust runs to be headless, but for debug runs to show the browser
                 if self.headless or self.headless is None and self.environment.runner is not None:
@@ -180,16 +259,16 @@ class PlaywrightScriptUser(PlaywrightUser):
 
         import mod  # type: ignore # pylint: disable-all
 
-        PlaywrightUser.pwrun = mod.run  # cant name it "run", because that collides with User.run
+        self.pwrun = mod.run  # cant name it "run", because that collides with User.run
 
     @task
     @pw
-    async def scriptrun(self):  # pylint: disable-all
-        await PlaywrightUser.pwrun(self.playwright)
+    async def scriptrun(self, page):  # pylint: disable-all
+        await self.pwrun(self.playwright)
 
 
 @events.test_start.add_listener
-def on_start(**_kwargs):
+def on_start(environment, **_kwargs):
     global loop
     loop = asyncio.new_event_loop()
     try:
@@ -199,8 +278,10 @@ def on_start(**_kwargs):
 
 
 @events.test_stop.add_listener
-def on_stop(**_kwargs):
+def on_stop(environment, **_kwargs):
     loop.stop()
+    # yappi.stop()
+    # yappi.get_func_stats().print_all()
     time.sleep(5)
 
 
