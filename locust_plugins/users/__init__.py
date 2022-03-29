d9113b9e3db71b4b104ee014550fa4fbc6a33a57
Lars Holmberg <lars.holmberg@svenskaspel.se>
2022-03-29 08:54:05 +0200

    note why we dont import playwright

--- locust_plugins/users/__init__.py
+++ locust_plugins/users/__init__.py
@@ -4,3 +4,6 @@ from .webdriver import *
 from .kafka import *
 from .rest import *
 from .mqtt import *
+
+# I'm scared by playwright.py:s side effects, otherwise we could do this:
+# from .playwright import *
