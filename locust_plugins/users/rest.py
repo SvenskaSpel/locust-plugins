dd0d35043572c73390fd2d93b8bce1df19074d41
Lars Holmberg <lars.holmberg@svenskaspel.se>
2022-08-17 09:52:37 +0200

    simplified import

--- locust_plugins/users/rest.py
+++ locust_plugins/users/rest.py
@@ -1,5 +1,5 @@
 from contextlib import contextmanager
-from locust.contrib.fasthttp import FastHttpUser
+from locust import FastHttpUser, HttpUser
 from locust.clients import ResponseContextManager
 import traceback
 import re
