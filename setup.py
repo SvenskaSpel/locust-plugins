2f2749aff1219de626ca396f1c4bb771b7c314be
Lars Holmberg <lars.holmberg@svenskaspel.se>
2023-10-26 08:06:01 +0200

    Forgot to list python 3.12 in setup.py

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Intended Audience :: Developers",
         "Intended Audience :: System Administrators",
     ],
