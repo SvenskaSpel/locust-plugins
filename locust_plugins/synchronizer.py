7b99a5e95d65b1963a807367892d133f9ab35d78
Lars Holmberg <lars.holmberg@svenskaspel.se>
2023-12-11 17:32:04 +0100

    refactor all the things.

--- locust_plugins/synchronizer.py
+++ locust_plugins/synchronizer.py
@@ -10,6 +10,22 @@ results: Dict[int, AsyncResult] = {}
 iterator: Optional[Iterator[Dict]] = None
 
 
+# received on master
+def _synchronizer_request(environment: Environment, msg, **kwargs):
+    assert iterator
+    data = next(iterator)
+    environment.runner.send_message(
+        "synchronizer_response",
+        {"payload": data, "user_id": msg.data["user_id"]},
+        client_id=msg.data["client_id"],
+    )
+
+
+# received on worker
+def _synchronizer_response(environment: Environment, msg, **kwargs):
+    results[msg.data["user_id"]].set(msg.data)
+
+
 def register(environment: Environment, reader: Optional[Iterator[dict]]):
     """Register synchronizer method handlers and tie them to use the iterator that you pass.
 
@@ -22,31 +38,15 @@ def register(environment: Environment, reader: Optional[Iterator[dict]]):
     if not reader and not isinstance(runner, WorkerRunner):
         raise Exception("reader is a mandatory parameter when not on a worker runner")
     if runner:
-        # called on master
-        def user_request(environment: Environment, msg, **kwargs):
-            assert iterator
-            data = next(iterator)
-            environment.runner.send_message(
-                "synchronizer_response",
-                {"payload": data, "user_id": msg.data["user_id"]},
-                client_id=msg.data["client_id"],
-            )
-
-        # called on worker
-        def user_response(environment: Environment, msg, **kwargs):
-            results[msg.data["user_id"]].set(msg.data)
-
-        if not isinstance(runner, WorkerRunner):
-            runner.register_message("synchronizer_request", user_request)
-        if not isinstance(runner, MasterRunner):
-            runner.register_message("synchronizer_response", user_response)
+        runner.register_message("synchronizer_request", _synchronizer_request)
+        runner.register_message("synchronizer_response", _synchronizer_response)
 
 
 def getdata(user: User) -> Dict:
     """Get the next data dict from reader
 
     Args:
-        u (User): current user object (we use the object id of the user to keep track of who's waiting for which data)
+        user (User): current user object (we use the object id of the User to keep track of who's waiting for which data)
     """
     if not user.environment.runner:  # no need to do anything clever if there is no runner
         return next(iterator)
