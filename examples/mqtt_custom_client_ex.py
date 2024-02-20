c7d79ca0db745e29312c5262a546283dfbb58910
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2024-02-20 13:38:07 +0200

    allowing more extensibility to customize the mqtt events logged in the database (#173)
    * allowing more extensibility in order to be able to customize the mqtt events logged in the database
    
    * fixed syntax issue
    
    * added example with simple custom Mqtt client implementation
    
    * removed unrequired override of initializer
    
    * removed unrequired dependencies

---
+++ examples/mqtt_custom_client_ex.py
@@ -0,0 +1,33 @@
+import time
+import typing
+
+from locust import task, TaskSet
+from locust_plugins.users.mqtt import MqttUser
+from locust_plugins.users.mqtt import MqttClient
+
+
+# extend the MqttClient class with your own custom implementation
+class MyMqttClient(MqttClient):
+
+    # you can override the event name with your custom implementation
+    def _generate_event_name(self, event_type: str, qos: int, topic: str):
+        return f"mqtt:{event_type}:{qos}"
+
+
+class MyUser(MqttUser):
+    # override the client_cls with your custom MqttClient implementation
+    client_cls: typing.Type[MyMqttClient] = MyMqttClient
+
+    @task
+    class MyTasks(TaskSet):
+        # Sleep for a while to allow the client time to connect.
+        # This is probably not the most "correct" way to do this: a better method
+        # might be to add a gevent.event.Event to the MqttClient's on_connect
+        # callback and wait for that (with a timeout) here.
+        # However, this works well enough for the sake of an example.
+        def on_start(self):
+            time.sleep(5)
+
+        @task
+        def say_hello(self):
+            self.client.publish("hello/locust", b"hello world")
