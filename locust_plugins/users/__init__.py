980c93b5601f8e593278087f91ec2e84e750e8ca
Lars Holmberg <lars.holmberg@redshirt.se>
2025-10-05 23:24:32 +0200

    Remove legacy import error catcher for imports from locust_plugins.users

--- locust_plugins/users/__init__.py
+++
@@ -1,6 +0,0 @@
-def __getattr__(name: str):
-    if name.endswith("User"):
-        raise ImportError(
-            f"locust_plugins.users no longer re-exports users. Use locust_plugins.users.{name.replace('User', '').lower()}.{name} instead"
-        )
-    raise ImportError(f"cannot import name '{name}' from 'locust_plugins.users'")
